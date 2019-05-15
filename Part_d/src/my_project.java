deaec81307d9610235ad1400d938d2fe38d85a2c
jadezee <jadezeewoldt@hotmail.com>
2019-05-15 20:06:04 +1200

    3rd line added- fav transportation

--- Part_d/src/my_project.java
+++ Part_d/src/my_project.java
@@ -4,6 +4,7 @@ public class my_project {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Jade Zeewoldt.");
 		System.out.println("My favourite colour is green.");
+		System.out.println("My favourite mode of transportation is by car.");
 	}
 
 }
