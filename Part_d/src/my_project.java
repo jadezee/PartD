88bcf83cbcaf937752a2b85256370aeecd291815
jadezee <jadezeewoldt@hotmail.com>
2019-05-15 20:05:02 +1200

    2nd line added- fav colour

--- Part_d/src/my_project.java
+++ Part_d/src/my_project.java
@@ -2,8 +2,8 @@
 public class my_project {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, my name is Jade Zeewoldt");
-
+		System.out.println("Hi, my name is Jade Zeewoldt.");
+		System.out.println("My favourite colour is green.");
 	}
 
 }
